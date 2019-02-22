9cdd9ee01586eedad04053f386877e50af657bb2
Blaok <i@blaok.me>
2019-02-21 23:02:16 -0800

    lab3: add build-essential in setup.sh

--- lab3/setup.sh
+++ lab3/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo env DEBIAN_FRONTEND=noninteractive apt install aria2 opencl-headers -y
+sudo env DEBIAN_FRONTEND=noninteractive apt install aria2 opencl-headers build-essential -y
 tmp=$(mktemp -d)
 pushd "$tmp"
 aria2c -x16 -c http://registrationcenter-download.intel.com/akdlm/irc_nas/vcp/13793/l_opencl_p_18.1.0.013.tgz
